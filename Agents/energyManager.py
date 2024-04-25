6ab8a953e2b74992c3711c7d0f10477dd67a1b97
João Castro <castrojoao839@gmail.com>
2024-04-25 01:20:30 +0100

    get data from openweather api

--- Agents/energyManager.py
+++ Agents/energyManager.py
@@ -5,23 +5,39 @@
 import random
 from spade import agent
 import time
+import geocoder
+import requests
 from aux_classes.Energy import Energy
 
 from Behaviours.InformEnergyProduced_behav import InformEnergyProduced_behav
 
+API_KEY = "a7cc9f91df9d8fbd6892c4ae81ea640e"
+URL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api}"
+
 class EnergySource():    
+        APIURL = ""
         sourcesAmount = {'solar': 0, 'wind': 0, 'hidrolics': 0}
-        possibleValues = {'solar' : {'Sunny' : (20,30), 'Cloudy': (2,7.5), 'Rainy': (0.4,1.5)}, 'wind' : {'low': (10,20), 'medium': (30,50), 'high': (60,80)}, 'hidrolics' : (1000,8000)}
+        #possibleValues = {'solar' : {'Sunny' : (20,30), 'Cloudy': (2,7.5), 'Rainy': (0.4,1.5)}, 'wind' : {'low': (10,20), 'medium': (30,50), 'high': (60,80)}, 'hidrolics' : (1000,8000)}
         
         def __init__(self, solarAmount, windAmount, hidrolicsAmount, cycleTime):
             print("Energy Source Agent starting...")
             self.sourcesAmount = {'solar': solarAmount, 'wind': windAmount, 'hidrolics': hidrolicsAmount}
             self.cycleTime = cycleTime
+            g = geocoder.ip('me')
+            lat, lon = g.latlng
+            self.APIURL = URL.format(lat=lat,lon=lon,api=API_KEY)
 
         def get_generatedEnergy(self):
+            r = requests.get(self.APIURL)
+            weather = r.json()
+            """
             solar = random.uniform(self.possibleValues['solar']['Sunny'][0], self.possibleValues['solar']['Sunny'][1])
             wind = random.uniform(self.possibleValues['wind']['medium'][0], self.possibleValues['wind']['medium'][1])
             hidrolics = random.uniform(self.possibleValues['hidrolics'][0], self.possibleValues['hidrolics'][1])
+            """
+            wind = weather["wind"]["speed"]
+            hidrolics = weather["main"]["humidity"]*10
+            solar = weather["main"]["temp"]/10
             energyProduced = Energy((solar * self.sourcesAmount['solar'] + wind * self.sourcesAmount['wind'] + hidrolics * self.sourcesAmount['hidrolics'] , random.randint(1,5)), self.cycleTime)
             return energyProduced
 
